3036a52ad9278e35b463bfcbe9afa627d0a6d3bc
Matt Terski <matt.terski@gmail.com>
2021-12-31 12:51:41 -0600

    day 7 part 1

---
+++ day07.js
@@ -0,0 +1,34 @@
+import fs from 'fs';
+
+const testValues = '16,1,2,0,4,2,7,1,2,14'.split(',').map((i) => Number(i));
+
+const values = fs
+    .readFileSync('./day07.txt', 'utf8')
+    .split(',')
+    .map((i) => Number(i));
+
+const fuelCost = (input, position) => {
+    return input.reduce(
+        (cost, current) => cost + Math.abs(current - position),
+        0
+    );
+};
+
+const optimize = (input) => {
+    const min = Math.min(...input);
+    const max = Math.max(...input);
+    let optimalCost, optimalPosition;
+
+    for (let ii = min; ii <= max; ii++) {
+        const cost = fuelCost(input, ii);
+        if (ii === min || cost < optimalCost) {
+            optimalCost = cost;
+            optimalPosition = ii;
+        }
+    }
+
+    return { optimalCost, optimalPosition };
+};
+
+optimize(testValues); //?
+optimize(values); //?
