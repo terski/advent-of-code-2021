af05d32d25b3d0fdd413cd0d5186433d1e5779f2
Matt Terski <matt.terski@gmail.com>
2022-01-08 18:07:58 -0600

    day 8 part 2

--- day08-2.js
+++ day08-2.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+
 const testPattern = `
 be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
 edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
@@ -13,26 +14,71 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
 
 const pattern = fs.readFileSync('./day08.txt', 'utf8');
 
-const uniqueOutputValues = (line) => {
-    return line
-        .split(' | ')[1]
-        .split(' ')
-        .reduce((count, outputValue) => {
-            if ([2, 3, 4, 7].includes(outputValue.length)) {
-                return count + 1;
-            }
-            return count;
-        }, 0);
+const normalize = (input) => {
+    return input.split('').sort().join('');
 };
 
-const count = (pattern) => {
+const matches = (s1, s2) => {
+    let count = 0;
+    for (let ii in s1) {
+        count += s2.includes(s1[ii]) ? 1 : 0;
+    }
+    return count;
+};
+
+const buildPatternMap = (signalPatterns) => {
+    const map = new Map();
+    map.set(1, signalPatterns.filter((p) => p.length === 2)[0]);
+    map.set(7, signalPatterns.filter((p) => p.length === 3)[0]);
+    map.set(4, signalPatterns.filter((p) => p.length === 4)[0]);
+    map.set(8, signalPatterns.filter((p) => p.length === 7)[0]);
+
+    signalPatterns.forEach((pattern) => {
+        switch (pattern.length) {
+            case 6:
+                if (matches(pattern, map.get(1)) === 1) {
+                    map.set(6, pattern);
+                } else if (matches(pattern, map.get(4)) === 3) {
+                    map.set(0, pattern);
+                } else {
+                    map.set(9, pattern);
+                }
+                break;
+            case 5:
+                if (matches(pattern, map.get(1)) === 2) {
+                    map.set(3, pattern);
+                } else if (matches(pattern, map.get(4)) === 2) {
+                    map.set(2, pattern);
+                } else {
+                    map.set(5, pattern);
+                }
+                break;
+        }
+    });
+    return new Map(Array.from(map, (i) => i.reverse()));
+};
+
+const outputValue = (line) => {
+    const signalPatterns = line.split(' | ')[0].split(' ').map(normalize);
+    const patternMap = buildPatternMap(signalPatterns);
+    const output = line
+        .split(' | ')[1]
+        .split(' ')
+        .map(normalize)
+        .map((i) => patternMap.get(i))
+        .join('');
+    return Number(output);
+};
+
+const sumOfOutputValues = (pattern) => {
     return pattern
         .split('\n')
         .filter((l) => l.length)
-        .reduce((count, line) => {
-            return count + uniqueOutputValues(line);
+        .reduce((sum, line) => {
+            const value = Number(outputValue(line));
+            return sum + value;
         }, 0);
 };
 
-count(testPattern); //?
-count(pattern); //?
+sumOfOutputValues(testPattern); //?
+sumOfOutputValues(pattern); //?
